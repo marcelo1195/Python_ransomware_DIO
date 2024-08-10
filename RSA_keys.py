e70eed1c0583e20b6e0dcf5074a2bd8a7a0ed6b1
marcelo <marcelo1195.mf>
2024-08-10 14:09:45 -0300

    RSA_keys.py refatorado utilizando biblioteas mais modernas

--- RSA_keys.py
+++ RSA_keys.py
@@ -1,22 +1,34 @@
-from Crypto.PublicKey import RSA
 import os
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
 def generate_and_save_keys():
+    #Create directory "keys" if not exists
     keys_dir = 'keys'
-    if not  os.path.exists(keys_dir):
+    if not os.path.exists(keys_dir):
         os.makedirs(keys_dir)
 
-    # Generating public and private keys
-    key = RSA.generate(4096)
+    #Generate keys
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+    public_key = private_key.public_key()
 
-    # Save private key
-    private_key = key.export_key()
+    # Saving private key
     with open(os.path.join(keys_dir, 'private_key.pem'), 'wb') as f:
-        f.write(private_key)
+        f.write(private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        ))
 
-    # Save public key
-    public_key = key.public_key().export_key()
-    with open(os.path.join(keys_dir, 'publick_key.pem'), 'wb') as f:
-        f.write(public_key)
+    # Saving public key
+    with open(os.path.join(keys_dir, 'public_key.pem'), 'wb') as f:
+        f.write(public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        ))
 
 if __name__ == "__main__":
     generate_and_save_keys()
