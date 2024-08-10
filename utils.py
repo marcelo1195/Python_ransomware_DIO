6648f69378ba24c561046333ed0f3d8eb46a3f08
marcelo <marcelo1195.mf>
2024-08-09 23:47:41 -0300

    função de iteração de diretórios atualizada para permitir uma lista de exclusão, função para capturar diretório atual do programa modelada

--- utils.py
+++ utils.py
@@ -1,12 +1,44 @@
 import os
 import platform
 
-def iterator(directory):
+def get_script_directory():
+    """Returns the directory where the script is located."""
+    return os.path.dirname(os.path.abspath(__file__))
+def iterator(directory, exclude_dirs=None, exclude_files=None, allowed_extensions=None):
+
+    """
+    :param directory:
+    :param exclude_dirs:
+    :param exclude_files:
+    :return List of file extensions allowed for
+    """
+    if exclude_dirs is None:
+        exclude_dirs = []
+    if exclude_files is None:
+        exclude_files = []
+    if allowed_extensions is None:
+        allowed_extensions = ['.txt', '.doc', '.pdf', '.jpg']
+
+    # Add current directory to exclude list
+    script_dir = get_script_directory()
+    exclude_dirs.append(script_dir)
+
     directory = directory
     archives = []
     for root, dirs, files in os.walk(directory):
+        dirs[:] = [d for d in dirs if os.path.join(root, d) not in exclude_dirs]
+
         for file in files:
-            archives.append(os.path.join(root, file))
+            file_path = os.path.join(root, file)
+
+            if file in exclude_files:
+                continue
+
+            if allowed_extensions and not any(file.endswith(ext) for ext in allowed_extensions):
+                continue
+
+            archives.append(file_path)
+
     return archives
 
 def get_operational_system():
