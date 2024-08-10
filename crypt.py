0f9122725bb54926a86f7421f900c0805ca02a65
marcelo <marcelo1195.mf>
2024-08-09 23:11:59 -0300

    arquivos de criptografia crypt.py e de decryptografia decrypt.py modelados

---
+++ crypt.py
@@ -0,0 +1,31 @@
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+
+
+def generate_fernet_key():
+    return Fernet.generate_key()
+
+def encrypt_fernet_key_with_rsa(fernet_key, public_key_path):
+    with open(public_key_path, "rb") as key_file:
+        public_key = load_pem_public_key(key_file.read())
+
+    encrypted_key = public_key.encrypt(
+        fernet_key,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA512()),
+            algorithm=hashes.SHA512(),
+            label=None
+        )
+    )
+    return encrypted_key
+
+def encrypt_file(file_path, fernet):
+    with open(file_path, 'rb') as file:
+        file_data = file.read()
+    encrypted_data = fernet.encrypt(file_data)
+    encrypt_file_path = file_path + '.encrypted'
+    with open(encrypt_file_path, 'wb') as file:
+        file.write(encrypted_data)
+    print(f"Archive encrypted: {encrypt_file_path}")
---
+++ decrypt.py
@@ -0,0 +1,34 @@
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+
+def decrypt_fernet_key_with_rsa(encrypted_fernet_key, private_key_path):
+    with open(private_key_path, "rb") as key_file:
+        private_key = load_pem_private_key(key_file.read(), password=None)
+
+    fernet_key = private_key.decrypt(
+        encrypted_fernet_key,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA512()),
+            algorithm=hashes.SHA512(),
+            label=None
+        )
+    )
+    return fernet_key
+
+def decrypt_file(file_path, fernet):
+    if not file_path.endswith('encrypted'):
+        print(f"Pulando arquivo não criptografado: {file_path}")
+        return
+
+    with open(file_path, 'rb') as file:
+        encrypted_data = file.read()
+    try:
+        decrypted_data = fernet.decrypt(encrypted_data)
+        decrypted_file_path = file_path[:-10]  # Remove '.encrypted'
+        with open(decrypted_file_path, 'wb') as file:
+            file.write(decrypted_data)
+        print(f"Arquivo descriptografado: {decrypted_file_path}")
+    except Exception as e:
+        print(f"Erro ao descriptografar {file_path}: {e}")
